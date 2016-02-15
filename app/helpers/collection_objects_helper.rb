79696875f7fbd65a80a853f20b9178d823d7daab
Mark Cooper <markchristophercooper@gmail.com>
2016-02-15 14:44:16 -0800

    Array fields link to search, resolves #9

--- app/helpers/collection_objects_helper.rb
+++ app/helpers/collection_objects_helper.rb
@@ -1,2 +1,10 @@
 module CollectionObjectsHelper
+  def field_to_s(collectionobject, attribute)
+    field = collectionobject.send(attribute.to_sym)
+    field = field.map(&:strip)
+      .reject(&:empty?)
+      .map{ |f| link_to f, search_path(field: attribute, query: f) }
+      .join(", ") if field.respond_to? :each
+    field.to_s.html_safe
+  end
 end
